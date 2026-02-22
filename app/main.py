52d94fe4b8318e55258cd24173ebe1b382e4460e
ferbol2018 <ferbol18@gmail.com>
2026-02-22 09:48:37 -0500

    cambio de url

--- app/main.py
+++ app/main.py
@@ -11,16 +11,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:49744",
-    "http://localhost",
-    "http://127.0.0.1:49744",
-]
+# origins = [
+#    "http://localhost:49744",
+#    "http://localhost",
+#    "http://127.0.0.1:49744",
+#]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "*"
+    "https://finanza-personal.netlify.app"
     ], #En PRO poner https://finanza-personal.netlify.app
     allow_credentials=True,
     allow_methods=["*"],
